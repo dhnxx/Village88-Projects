78cee25691f5b8cca8a37008fa26884a76ea5fbd
Dhancovic Ivan Mendoza <dhancomendoza@gmail.com>
2024-02-11 00:51:13 +0800

    Update Template

--- 02-PHP/Z1-CodeIgniter/application/views/partials/header.php
+++ 02-PHP/Z1-CodeIgniter/application/views/partials/header.php
@@ -5,5 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url("assets/css/normalize.css")?>">
     <link rel="stylesheet" href="<?= base_url("assets/css/main.css?v" . time()) ?>">
 </head>
